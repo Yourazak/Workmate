c2dadb21111c212d5d2ae82d1bbe87a202a4f0f1
Albina Mollander <albinamo@student.21-school.ru>
2025-06-23 20:24:13 +0300

    README

--- go.mod
+++ go.mod
@@ -1,1 +1,7 @@
 module Workmate
+
+go 1.24.0
+
+require github.com/google/uuid v1.6.0
+
+require github.com/go-chi/chi/v5 v5.2.2 // indirect
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"Workmate/handlers"
+	"Workmate/storage"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	store := storage.NewTaskStorage()
+	handler := handlers.NewTaskHandler(store)
+
+	r := chi.NewRouter()
+
+	r.Post("/task", handler.CreateTask)
+	r.Get("/task", handler.GetTask)
+	r.Delete("/task", handler.DeleteTask)
+
+	log.Println("Server running on :8080")
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+}
